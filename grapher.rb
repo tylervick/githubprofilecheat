afd7eb55840d501a2bd152b6446cc633e75d526d
Tyler Vick <tyler@tylervick.com>
2024-12-20 11:17:19 -0700

    thank you will
    Signed-off-by: Tyler Vick <tyler.vick.-nd@disney.com>

--- grapher.rb
+++ grapher.rb
@@ -9,13 +9,13 @@ require 'date'
 # █████████████████████████
 
 PATTERN = <<-EOF.split("\n").map{|line| line.split(//)}.transpose.map(&:join).join
-XXXXXXXXXXXXXXXXXXXXXXXXX
-X XXX X     X  XXXX  XXXX
-X XXX XXX XXX  XXXX  XXXX
-X X X XXX XXX  XXXX  XXXX
-X X X XXX XXX  XXXX  XXXX
-X     X     X     X     X
-XXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+X     X  X  X  XXXX     X     X
+XXX XXX  X  X  XXXX  XXXX  XX X
+XXX XXXX   XX  XXXX    XX  X XX
+XXX XXXXX XXX  XXXX  XXXX  X  X
+XXX XXXXX XXX     X     X  X  X
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 EOF
 
 MASK = PATTERN.split(//).map{|c| c == 'X'}
@@ -48,6 +48,6 @@ str_commit_dates = commit_dates.map(&:to_s)
 commit_dates.each do |date|
   puts date
   File.open('random_list_of_dates', 'w') { |f| f << str_commit_dates.shuffle.first(12).join("\n") }
-  `GIT_AUTHOR_DATE="#{date}" GIT_COMMITTER_DATE="#{date}" git commit -am "#{date}"`
+  `GIT_AUTHOR_NAME="Tyler Vick" GIT_AUTHOR_EMAIL="tyler@tylervick.com" GIT_AUTHOR_DATE="#{date}" GIT_COMMITTER_DATE="#{date}" git commit -sam "#{date}"`
 end
 
